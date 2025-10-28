53936b9ca86bb5220587798e705a01ac8cff68cc
linahsan <lina.sanchez.nyc@gmail.com>
2025-10-27 22:04:50 -0400

    app.js modified for chore routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,15 +3,26 @@ import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import CreateGroup from './pages/CreateGroup';
 import EditGroup from './pages/EditGroup';
+import ChoresDashboard from './pages/ChoresDashboard';
+import AddChore from './pages/AddChore';
+import EditChore from './pages/EditChore';
 
 export default function App() {
   return (
     <Layout>
       <Routes>
+
+        {/*Group Home Routes*/}
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/groups/new" element={<CreateGroup />} />
         <Route path="/groups/:groupId/edit" element={<EditGroup />} />
+
+        {/*Chores Routes*/}
+        <Route path="/chores" element={<ChoresDashboard />} />
+        <Route path="/add-chore" element={<AddChore />} />
+        <Route path="/chores/:choreId/edit" element={<EditChore />} />
+
       </Routes>
     </Layout>
   );
